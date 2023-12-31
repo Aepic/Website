264883e44a023526468cba9673d60b3fff9dcd65
ItsEricSun <ericsun828@gmail.com>
2023-12-30 23:31:29 -0500

    add users to frontend

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ const App = () => {
   return (
     <div>
       <p>Hello World!</p>
-        <Post />
+      <Post />
     </div>
   );
 };
--- client/src/components/post.js
+++ client/src/components/post.js
@@ -5,6 +5,14 @@ const Post = () => {
   useEffect(() => {
     fetch("http://localhost:5000/post")
       .then((response) => response.json())
+      .then((data) => {
+        const promises = data.map((post) =>
+          fetch(`http://localhost:5000/user/${post.user_id}`)
+            .then((response) => response.json())
+            .then((user) => ({ ...post, user }))
+        );
+        return Promise.all(promises);
+      })
       .then((data) => setPosts(data))
       .catch((error) => console.error("Error fetching data:", error));
   }, []);
@@ -13,9 +21,10 @@ const Post = () => {
     <div>
       <h1>Posts</h1>
       <ul>
-        {posts.map(post => (
+        {posts.map((post) => (
           <li key={post._id}>
             <p>{post.prompt}</p>
+            <p>Posted by: {post.user ? post.user.username : "Anonymous"}</p>
           </li>
         ))}
       </ul>
