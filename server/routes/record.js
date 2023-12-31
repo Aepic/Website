1fbe907c42cf1331ab2444e6228d0c6dde23bd66
ItsEricSun <ericsun828@gmail.com>
2023-12-30 23:13:57 -0500

    update record.js

--- server/routes/record.js
+++ server/routes/record.js
@@ -1,47 +1,53 @@
 const express = require("express");
- 
+
 const recordRoutes = express.Router();
 const dbo = require("../db/conn");
 const ObjectId = require("mongodb").ObjectId;
- 
- 
+
 // Get a list of all the posts.
 recordRoutes.route("/post").get(function (req, res) {
- let db_connect = dbo.getDb("aepic");
- db_connect
-   .collection("posts")
-   .find({})
-   .toArray(function (err, result) {
-     if (err) throw err;
-     res.json(result);
-   });
+  let db_connect = dbo.getDb();
+  db_connect.collection("post").find({}).toArray()
+    .then(result => res.json(result))
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ error: "Internal Server Error" });
+    });
 });
- 
+
 // Get a single post by id
 recordRoutes.route("/post/:id").get(function (req, res) {
- let db_connect = dbo.getDb();
- let myquery = { _id: ObjectId(req.params.id) };
- db_connect
-   .collection("posts")
-   .findOne(myquery, function (err, result) {
-     if (err) throw err;
-     res.json(result);
-   });
+  let db_connect = dbo.getDb();
+  let myquery = { _id: ObjectId(req.params.id) };
+  
+  db_connect.collection("post").findOne(myquery)
+    .then(result => {
+      res.json(result);
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ error: "Internal Server Error" });
+    });
 });
- 
+
 // Create a new post.
 recordRoutes.route("/post/add").post(function (req, response) {
- let db_connect = dbo.getDb();
- let myobj = {
-   user_id: req.body.user_id,
-   prompt: req.body.prompt,
-   image_url: req.body.image_url,
-   timestamp_creation: req.body.timestamp_creation,
- };
- db_connect.collection("posts").insertOne(myobj, function (err, res) {
-   if (err) throw err;
-   response.json(res);
- });
+  let db_connect = dbo.getDb();
+  let myobj = {
+    user_id: req.body.user_id,
+    prompt: req.body.prompt,
+    image_url: req.body.image_url,
+    timestamp_creation: req.body.timestamp_creation,
+  };
+  
+  db_connect.collection("post").insertOne(myobj)
+    .then(res => {
+      response.json(res);
+    })
+    .catch(err => {
+      console.error(err);
+      response.status(500).json({ error: "Internal Server Error" });
+    });
 });
 
 module.exports = recordRoutes;
